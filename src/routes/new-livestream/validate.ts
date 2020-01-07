979853d21d87651b18ba4860d7854008d48971a2
Alexander Polovnikov <aleksandrpol@gmail.com>
2020-01-07 13:04:16 +0300

    vid-192

--- src/routes/new-livestream/validate.ts
+++ src/routes/new-livestream/validate.ts
@@ -1,7 +1,9 @@
 import * as Yup from 'yup';
 
 export default Yup.object().shape({
-  name: Yup.string().required(),
+  name: Yup.string()
+    .max(255, 'Stream name must be shorter than 255 characters')
+    .required(),
   inputType: Yup.string().required(),
   outputType: Yup.string().required(),
   profile: Yup.object().required(),

a23e2c51203ec01472c630b824f9ebc6295eec1b
Alexander <aleksandrpol@gmail.com>
2020-03-31 18:07:38 +0300

    add feature settings to profile

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { map } from 'lodash/fp';
 import { IconName, Navigation as BaseNavigation } from 'ui-kit';
 import UserStore from 'stores/user';
 import NavLink from './NavLink';
 import css from './index.module.scss';
-import { USER_ROLE } from '../../const';
 
 const navIcons: Record<string, IconName> = {
   streams: 'pipelines',
   workers: 'workers',
   account: 'account',
-  wallet: 'videoCoinWallet',
-};
-
-const navLinks: Record<USER_ROLE, string[]> = {
-  [USER_ROLE.REGULAR]: ['streams', '', 'billing', 'account'],
-  [USER_ROLE.MINER]: ['workers', '', 'billing', 'account'],
-  [USER_ROLE.SUPER]: ['streams', 'workers', '', 'billing', 'account'],
+  billing: 'billing',
 };
 
 const navLabels: Record<string, string> = {
   streams: 'Streams',
   workers: 'Workers',
-  wallet: 'Wallet',
+  billing: 'Billing',
   account: 'Account',
 };
 
 const Navigation = () => {
-  const { isActive, user } = UserStore;
+  const { isActive, isWorker, isPublisher } = UserStore;
+  const navLinks = (): string[] => {
+    if (isWorker && isPublisher) {
+      return ['streams', 'workers', '', 'billing', 'account'];
+    }
+    if (isPublisher) {
+      return ['streams', '', 'billing', 'account'];
+    }
+
+    return ['workers', '', 'billing', 'account'];
+  };
   const renderLink = (link: string) => {
     const path = `/dashboard/${link}`;
 
@@ -47,9 +51,9 @@ const Navigation = () => {
 
   return (
     <BaseNavigation>
-      {isActive && <>{map(renderLink)(navLinks[user.role])}</>}
+      {isActive && <>{map(renderLink)(navLinks())}</>}
     </BaseNavigation>
   );
 };
 
-export default Navigation;
+export default observer(Navigation);

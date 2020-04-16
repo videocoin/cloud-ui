94bc49432125890e20d2e1cfbdec34f8cffb48d5
Alexander <aleksandrpol@gmail.com>
2020-04-16 13:04:28 +0300

    add age to payments table

--- src/helpers/timeAgo.ts
+++ src/helpers/timeAgo.ts
@@ -4,7 +4,7 @@ import en from 'javascript-time-ago/locale/en';
 TimeAgo.addLocale(en);
 const lTimeAgo = new TimeAgo('en-US');
 
-function timeAgo(date: string): string {
+function timeAgo(date: string | number): string {
   return lTimeAgo
     .format(new Date(date), { flavour: 'short' })
     .split('.')

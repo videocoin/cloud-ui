38545c2dbfdeba0a2bbe31955356bf8587db8d21
Alexander <aleksandrpol@gmail.com>
2020-05-06 19:26:47 +0300

    fix earnings table

--- src/components/Earnings/TableRow.tsx
+++ src/components/Earnings/TableRow.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Typography } from 'ui-kit';
 import WorkerStatus from 'components/Workers/WorkerStatus';
 import { IWorker } from 'stores/workers';
+import css from './styles.module.scss';
 
 const TableRow = ({ reward, name, status }: IWorker) => {
   return (
     <tr>
-      <td>
+      <td className={css.statusCell}>
         <WorkerStatus status={status} name={status} />
       </td>
       <td>
